24fce208f66a3326f8b5df190e72208f0592797d
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-03-02 18:24:15 +0530

    implemented new workout methods

--- script.js
+++ script.js
@@ -23,10 +23,54 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+/////////////////////////////////////////////////
+class Workout {
+    date = new Date();
+    id = (Date.now() + "").slice(-10);
+
+    constructor(coords, distance, duration) {
+        this.coords = coords; //[lat,lng]
+        this.distance = distance; //km
+        this.duration = duration; //minutes
+    }
+}
+//////////////////////////////////
+class Running extends Workout {
+    type = "running";
+
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+    calcPace() {
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+// //////////////////////////////
+class Cycling extends Workout {
+    type = "cycling";
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+    calcSpeed() {
+        this.speed = this.distance / (this.duration / 60); //km/hr
+        return this.speed;
+    }
+}
+// const r1 = new Running([73, 82], 83, 93, 29);
+// const c1 = new Cycling([73, 82], 83, 93, 29);
+// console.log(r1, c1);
+
+///////////////////////////////////////////////////
 class App {
     // private fields
     #map;
     #mapEvent;
+    #workouts = [];
 
     constructor() {
         this._getPosition();
@@ -83,15 +127,61 @@ class App {
     _newWorkout(e) {
         e.preventDefault();
 
+        //validation helper functions
+        const validInputs = (...inputs) =>
+            inputs.every((inp) => Number.isFinite(inp));
+        const checkPositive = (...inputs) => inputs.every((inp) => inp > 0);
+
+        //get data from form
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+        const { lat, lng } = this.#mapEvent.latlng;
+        let workout;
+
+        if (type === "running") {
+            const cadence = +inputCadence.value;
+
+            //data validation
+            if (
+                !validInputs(distance, duration, cadence) ||
+                !checkPositive(distance, duration, cadence)
+            )
+                return alert("Enter a positive number!");
+
+            workout = new Running([lat, lng], distance, duration, cadence);
+        }
+        if (type === "cycling") {
+            const elevation = +inputElevation.value;
+            //data validation
+            if (
+                !validInputs(distance, duration, elevation) ||
+                !checkPositive(distance, duration)
+            )
+                return alert("Enter a positive number!");
+
+            workout = new Cycling([lat, lng], distance, duration, elevation);
+        }
+
+        //adding new workout to workouts array
+        // console.log(workout);
+        this.#workouts.push(workout);
+
         // clearing fields
         inputCadence.value =
             inputDistance.value =
             inputDuration.value =
             inputElevation.value =
                 "";
+
         // display marker
-        const { lat, lng } = this.#mapEvent.latlng;
-        L.marker([lat, lng])
+        this.displayMarker(workout);
+        //hiding the form again
+        form.classList.add("hidden");
+    }
+
+    displayMarker(workout) {
+        L.marker(workout.coords)
             .addTo(this.#map)
             .bindPopup(
                 L.popup({
@@ -99,13 +189,11 @@ class App {
                     minWidth: 50,
                     autoClose: false,
                     closeOnClick: false,
-                    className: "running-popup",
+                    className: `${workout.type}-popup`,
                 })
             )
             .setPopupContent("Working")
             .openPopup();
-
-        form.classList.add("hidden");
     }
 }
 
